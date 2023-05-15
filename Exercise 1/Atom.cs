03de1b483e6a1b4f54b596998fb0e116b0a6290a
THANHLE510 <111995033+THANHLE510@users.noreply.github.com>
2023-05-15 11:57:07 +0700

    Add files via upload

---
+++ Exercise 1/Atom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Atom
+    {
+        private int number;
+        private string symbol;
+        private string fullname;
+        private float weight;
+        
+
+        public bool Accept()
+        {
+            System.Console.WriteLine("Enter atomic number: ");
+            if(!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Invalid input.Please enter an interger for atomic number.");
+                return false;
+            }
+
+            System.Console.WriteLine("Enter symbol: ");
+            symbol = System.Console.ReadLine();
+
+            System.Console.WriteLine("Enter full name: "); 
+            fullname = System.Console.ReadLine();
+
+            System.Console.WriteLine("Enter automic weight: ");
+            if(!float.TryParse(Console.ReadLine(), out weight))
+            {
+                Console.WriteLine("Invalid input.Please enter a floating-point value atomic weight.");
+                return false;
+            }
+            return true;
+        }
+        public void Display()
+        {
+            System.Console.WriteLine("Atomic number: " + number);
+            System.Console.WriteLine("Atomic symbol: " + symbol);
+            System.Console.WriteLine("Full name: " + fullname);
+            System.Console.WriteLine("Atomic weight: " + weight);
+            System.Console.WriteLine();
+        }
+    }
+}
---
+++ Exercise 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Atom[] atoms = new Atom[10];
+            int count = 0;
+
+            while (count < 10)
+            {
+                Atom atom = new Atom();
+                if(atom.Accept())
+                {
+                    atoms[count] = atom;    
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.Please try again");
+                }
+            }
+            Console.WriteLine("Automic Information");
+            Console.WriteLine(".....................");
+            for(int i = 0; i < count;i++) 
+            {
+                atoms[i].Display();
+            }
+        }
+    }
+}
